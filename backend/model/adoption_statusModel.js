9a3a839157c052b90d0164694b66747be1bc48ff
Josh Brian Bugarin <jofe.bugarin.up@phinmaed.com>
2024-10-09 15:40:15 +0800

    Fixed adoption status error

--- backend/model/adoption_statusModel.js
+++ backend/model/adoption_statusModel.js
@@ -4,11 +4,7 @@ const Schema = mongoose.Schema;
 const adoptionStatusSchema = new Schema({
   adoptionRequestId: { type: Schema.Types.ObjectId, ref: 'AdoptionRequest', required: true }, 
   adopterId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  adopteeId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true, 
-    ref: 'Adoptee'
-  },
+  adopteeId: { type: Schema.Types.ObjectId, ref: 'User', required: true }, 
   petId: { type: Schema.Types.ObjectId, ref: 'Pet', required: true }, 
   requestDate: { type: Date, default: Date.now }, 
   status: { type: String, default: 'Pending' }, 
