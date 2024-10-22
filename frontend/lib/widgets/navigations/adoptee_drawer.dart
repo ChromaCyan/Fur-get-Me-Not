6865995f6b8340f2824bcf55e69193d3328f8d98
Joaquin Gabriel Camangeg <joaquingabrielcamangeg6@gmail.com>
2024-10-23 07:43:47 +0800

    Drawers Updated

--- frontend/lib/widgets/navigations/adoptee_drawer.dart
+++ frontend/lib/widgets/navigations/adoptee_drawer.dart
@@ -3,11 +3,12 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fur_get_me_not/authentication/screen/login_screen.dart';
 import 'package:fur_get_me_not/adoptee/screens/adoptee_profile/adoptee_profile.dart';
 import 'package:fur_get_me_not/adoptee/repositories/pet_management/admin_pet_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fur_get_me_not/shared/blocs/profile_bloc.dart';
 
 class AdopteeDrawer extends StatelessWidget {
   final FlutterSecureStorage _storage = FlutterSecureStorage();
-  final AdminPetRepository _petRepository =
-      AdminPetRepository(); // Instance of repository
+  final AdminPetRepository _petRepository = AdminPetRepository();
 
   AdopteeDrawer({Key? key}) : super(key: key);
 
@@ -25,7 +26,7 @@ class AdopteeDrawer extends StatelessWidget {
 
   Future<void> _downloadExcel(BuildContext context) async {
     try {
-      await _petRepository.downloadExcel(); // Call the download method
+      await _petRepository.downloadExcel();
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Pet table downloaded successfully!')),
       );
@@ -39,52 +40,138 @@ class AdopteeDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: const Text(
-              'Fur Get Me Not',
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text('Profile'),
-            onTap: () async {
-              final userId = await _getUserId();
-              if (userId != null) {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => AdopteeProfilePage(userId: userId),
-                  ),
+      child: Container(
+        color: const Color(0xFF21899C),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            SizedBox(height: 45),
+            // const DrawerHeader(
+            //   decoration: BoxDecoration(
+            //     color: Colors.transparent,
+            //   ),
+            //   child: Text(
+            //     'Fur Get Me Not',
+            //     style: TextStyle(
+            //       color: Colors.white,
+            //       fontSize: 24,
+            //     ),
+            //   ),
+            // ),
+            FutureBuilder<String?>(
+              future: _getUserId(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                }
+                final userId = snapshot.data;
+                if (userId != null) {
+                  return BlocBuilder<ProfileBloc, ProfileState>(
+                    builder: (context, state) {
+                      if (state is ProfileLoading) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (state is ProfileLoaded) {
+                        final profile = state.profileData;
+                        return _buildProfileSection(profile);
+                      } else {
+                        return const Center(
+                          child: Text('Unable to load profile.'),
+                        );
+                      }
+                    },
+                  );
+                }
+                return const Center(
+                  child: Text('User not found. Please log in again.'),
                 );
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('User not found. Please log in again.'),
-                  ),
-                );
-              }
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.download),
-            title: const Text('Download Pet Table'),
-            onTap: () => _downloadExcel(context), // Call the download function
-          ),
-          ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text('Logout'),
-            onTap: () async {
-              await _logout(context);
-            },
-          ),
-        ],
+              },
+            ),
+            _buildListTile(
+              icon: Icons.person,
+              text: 'Profile',
+              onTap: () async {
+                final userId = await _getUserId();
+                if (userId != null) {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => AdopteeProfilePage(userId: userId),
+                    ),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('User not found. Please log in again.'),
+                    ),
+                  );
+                }
+              },
+            ),
+            _buildListTile(
+              icon: Icons.download,
+              text: 'Download Pet Table',
+              onTap: () => _downloadExcel(context),
+            ),
+            _buildListTile(
+              icon: Icons.logout,
+              text: 'Logout',
+              onTap: () async {
+                await _logout(context);
+              },
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildProfileSection(Map<String, dynamic> profile) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 50,
+          backgroundImage: profile['profileImage'] != null &&
+                  profile['profileImage'].isNotEmpty
+              ? NetworkImage(profile['profileImage']) as ImageProvider
+              : const AssetImage('assets/default_profile.png'),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          '${profile['firstName']} ${profile['lastName']}',
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        const SizedBox(height: 4),
+        const Text(
+          'Adoptee',
+          style: TextStyle(
+            color: Colors.white70,
+            fontSize: 14,
+          ),
+        ),
+        const Divider(
+            color: Colors.white70, thickness: 1, indent: 16, endIndent: 16),
+      ],
+    );
+  }
+
+  Widget _buildListTile({
+    required IconData icon,
+    required String text,
+    required VoidCallback onTap,
+  }) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.white),
+      title: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: onTap,
     );
   }
 }
--- frontend/lib/widgets/navigations/drawer.dart
+++ frontend/lib/widgets/navigations/drawer.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:fur_get_me_not/adopter/repositories/adoption_list/pet_repository.dart';
 import 'package:fur_get_me_not/authentication/screen/login_screen.dart';
 import 'package:fur_get_me_not/adopter/screens/adopter_profile/adopter_profile.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fur_get_me_not/shared/blocs/profile_bloc.dart';
 
 class AppDrawer extends StatelessWidget {
-  final FlutterSecureStorage _storage = FlutterSecureStorage();
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+  final PetRepository _petRepository = PetRepository();
 
   AppDrawer({Key? key}) : super(key: key);
 
@@ -22,52 +26,154 @@ class AppDrawer extends StatelessWidget {
     return await _storage.read(key: 'userId');
   }
 
+  // Future<void> _downloadExcel(BuildContext context) async {
+  //   try {
+  //     await _petRepository.downloadExcel();
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       const SnackBar(content: Text('Pet table downloaded successfully!')),
+  //     );
+  //   } catch (e) {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(content: Text('Failed to download pet table: $e')),
+  //     );
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Text(
-              'Fur Get Me Not',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-              ),
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Profile'),
-            onTap: () async {
-              final userId = await _getUserId();
-              if (userId != null) {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => ProfilePage(userId: userId),
-                  ),
+      child: Container(
+        color: const Color(0xFF21899C),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            SizedBox(height: 45),
+            // const DrawerHeader(
+            //   decoration: BoxDecoration(
+            //     color: Colors.transparent,
+            //   ),
+            //   child: Text(
+            //     'Fur Get Me Not',
+            //     style: TextStyle(
+            //       color: Colors.white,
+            //       fontSize: 24,
+            //     ),
+            //   ),
+            // ),
+            FutureBuilder<String?>(
+              future: _getUserId(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                }
+                final userId = snapshot.data;
+                if (userId != null) {
+                  return BlocBuilder<ProfileBloc, ProfileState>(
+                    builder: (context, state) {
+                      if (state is ProfileLoading) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (state is ProfileLoaded) {
+                        final profile = state.profileData;
+                        return _buildProfileSection(profile);
+                      } else {
+                        return const Center(
+                          child: Text('Unable to load profile.'),
+                        );
+                      }
+                    },
+                  );
+                }
+                return const Center(
+                  child: Text('User not found. Please log in again.'),
                 );
-              } else {
-                // Handle case where userId is null, e.g., show an error message
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('User not found. Please log in again.')),
-                );
-              }
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.logout),
-            title: Text('Logout'),
-            onTap: () async {
-              await _logout(context);
-            },
-          ),
-        ],
+              },
+            ),
+            _buildListTile(
+              icon: Icons.person,
+              text: 'Profile',
+              onTap: () async {
+                final userId = await _getUserId();
+                if (userId != null) {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ProfilePage(userId: userId),
+                    ),
+                  );
+                } else {
+                  // Handle case where userId is null, e.g., show an error message
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                        content: Text('User not found. Please log in again.')),
+                  );
+                }
+              },
+            ),
+            // _buildListTile(
+            //   icon: Icons.download,
+            //   text: 'Download Pet Table',
+            //   onTap: () => _downloadExcel(context),
+            // ),
+            _buildListTile(
+              icon: Icons.logout,
+              text: 'Logout',
+              onTap: () async {
+                await _logout(context);
+              },
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildProfileSection(Map<String, dynamic> profile) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 50,
+          backgroundImage: profile['profileImage'] != null &&
+                  profile['profileImage'].isNotEmpty
+              ? NetworkImage(profile['profileImage']) as ImageProvider
+              : const AssetImage('assets/default_profile.png'),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          '${profile['firstName']} ${profile['lastName']}',
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        const SizedBox(height: 4),
+        const Text(
+          'Adoptee',
+          style: TextStyle(
+            color: Colors.white70,
+            fontSize: 14,
+          ),
+        ),
+        const Divider(
+            color: Colors.white70, thickness: 1, indent: 16, endIndent: 16),
+      ],
+    );
+  }
+
+  Widget _buildListTile({
+    required IconData icon,
+    required String text,
+    required VoidCallback onTap,
+  }) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.white),
+      title: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: onTap,
     );
   }
 }
